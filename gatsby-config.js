aef8da7f444739c1cf76ef99df7886c6b9e746a8
Caiheng111 <ianyaheng822@gmail.com>
2023-10-05 21:54:22 +1100

    update project with js project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,11 @@
-import type { GatsbyConfig } from "gatsby";
-
-const config: GatsbyConfig = {
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
   siteMetadata: {
     title: `My Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
   plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
@@ -30,5 +27,3 @@ const config: GatsbyConfig = {
     __key: "pages"
   }]
 };
-
-export default config;
