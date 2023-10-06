41367659b8c011139674cd694b68c0979c8dbee6
Caiheng111 <ianyaheng822@gmail.com>
2023-10-06 13:16:18 +1100

    feature/layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,28 +2,50 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `My Gatsby Site`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-mdx", "gatsby-transformer-remark", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    siteMetadata: {
+        title: `My Gatsby Site`,
+        siteUrl: `https://www.yourdomain.tld`
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
-};
+    plugins: [
+        "gatsby-plugin-styled-components",
+        "gatsby-plugin-image",
+        `gatsby-plugin-sass`,
+        "gatsby-plugin-sitemap",
+        {
+            resolve: "gatsby-plugin-manifest",
+            options: {
+                icon: "src/images/icon.png"
+            }
+        },
+        {
+            resolve: `gatsby-alias-imports`,
+            options: {
+                aliases: {
+                    "@styles": `src/styles`,
+                    "@components": `src/components`,
+                    "@assets": `src/assets`
+                }
+            }
+        },
+        "gatsby-plugin-mdx",
+        "gatsby-transformer-remark",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "./src/images/"
+            },
+            __key: "images"
+        },
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "pages",
+                path: "./src/pages/"
+            },
+            __key: "pages"
+        }
+    ]
+}
---
+++ src/components/layout/Layout.js
@@ -0,0 +1,56 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React, { useEffect } from "react"
+import PropTypes from "prop-types"
+
+// import Navigation from "@components/layout/Navigation"
+// import Overlay from "@components/layout/Overlay"
+// import PageProgress from "@components/layout/PageProgress"
+// import SocialDrawer from "@components/social/SocialDrawer"
+// import ThemeProvider from "../theme/ThemeProvider"
+import "../../styles/main.scss"
+
+const Layout = ({ children }) => {
+    useEffect(() => {
+        /** Set Scroll Event Listener */
+        document.addEventListener("scrollStart", e => {
+            document.title = e.detail
+                ? `${e.detail.anchor.dataset.name} | Lu-Vuong Le`
+                : null
+        })
+
+        /** Scroll to Anchor Section if any */
+        const hash = window.location.hash
+
+        if (hash) {
+            const section = document.getElementById(hash.replace("#", ""))
+            section.scrollIntoView({ behavior: "smooth" })
+        }
+
+        return () => {}
+    }, [])
+
+    return (
+        <div className="main">
+            {/* <PageProgress /> */}
+            {/* <Navigation /> */}
+            <main id="main" className="">
+                {/* <Overlay name="overlay-main" /> */}
+                {children}
+            </main>
+            <footer></footer>
+            {/* <SocialDrawer /> */}
+        </div>
+    )
+}
+
+Layout.propTypes = {
+    children: PropTypes.node.isRequired
+}
+
+export default Layout
